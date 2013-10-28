09fdfc26c4e0dd55fff82cf1494de5ff1b05f172
Basia <rypczyk@gmail.com>
2013-10-28 07:44:33 +0100

    Poprawka do restartowanych serwisów

--- src/apply_etc.sh
+++ src/apply_etc.sh
@@ -6,7 +6,7 @@ rsync -vr config/* /
 echo "Restartuje usługi"
 
 # TODO: Można by jakoś wykrywać, które usługi restartować po nadgraniu konfiguracji...
-for D in nginx postgresql redis-server
+for D in nagios3
 	do
 		service $D restart
 done
